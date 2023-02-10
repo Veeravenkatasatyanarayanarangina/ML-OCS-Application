e6bf9b0790c10b060d9f9198e4acfd852bab98b0
Veera venkata satyanarayana <satyanarayanarangina@gmail.com>
2023-02-10 14:20:58 +0530

    ML-OCS Application

---
+++ src/main/java/com/motivity/ocs/MotivityHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.motivity.ocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivityHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivityHospitalApplication.class, args);
+	}
+
+}
